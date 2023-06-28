51f11c71043f4208e14a817ea25607422f0fa95e
1136623363 <1136623363@qq.com>
2023-06-28 22:48:12 +0800

    2.6

--- NameCheck.py
+++ NameCheck.py
@@ -6,17 +6,17 @@ def namecheck(text):
     p = Pinyin()
 
     # 要处理的文本
-    #text = "习近平主席在会议上发表了重要讲话。中共中央决定对某些地方的腐败问题进行严肃查处。"
+    #text = ""
 
     # 定义敏感词列表
     leaders = []
     sensitive_words = []
 
-    # 构建正则表达式，用于匹配中共领导人姓名和其他敏感词
+    # 构建正则表达式，用于匹配敏感词
     pattern = "|".join(leaders + sensitive_words)
     regex = re.compile(pattern)
 
-    # 检测文本中的中共领导人姓名和其他敏感词，并将其转换为拼音首字母缩写
+    # 检测文本中的敏感词，并将其转换为拼音首字母缩写
     def replace_sensitive(match):
         return "".join([p.get_initials(match.group()), ""])
 
